ac279948d208cc67ac64befe54616b40e1456a76
prabeshadhikari <81055107+prabeshadhikari@users.noreply.github.com>
2024-04-07 09:12:13 +1000

    read

---
+++ read.cs
@@ -0,0 +1,11 @@
+using System;
+namespace myApplication
+{
+ class Program{
+    static void Main (string[] args){
+        Console.WriteLine("Enter username: ");
+        string userName = Console.ReadLine();
+        Console.WriteLine("Your username is " + userName);
+    }
+ }   
+}
